cb72f13e3403524d819d41a00efbe076f925744a
JuanCarrion <Juanpablocarriondiaz@gmail.com>
2024-02-19 18:50:40 -0600

    p7.4

--- pages/index.js
+++ pages/index.js
@@ -192,63 +192,6 @@ modals.forEach((modal) => {
 /*                                  Validation                             */
 /* -----------------------------------------------------------------------------*/
 
-function showInputError(formEl, inputEl, { inputErrorClass, errorClass }) {
-  const errorMessageEl = formEl.querySelector("#" + inputEl.id + "-error");
-  inputEl.classList.add(inputErrorClass);
-  errorMessageEl.textContent = inputEl.validationMessage;
-  errorMessageEl.classList.add(errorClass);
-}
-
-function hideInputError(formEl, inputEl, { inputErrorClass, errorClass }) {
-  const errorMessageEl = formEl.querySelector("#" + inputEl.id + "-error");
-  inputEl.classList.remove(inputErrorClass);
-  errorMessageEl.textContent = "";
-  errorMessageEl.classList.remove(errorClass);
-}
-
-function checkInputValidity(formEl, inputEl, options) {
-  if (!inputEl.validity.valid) {
-    return showInputError(formEl, inputEl, options);
-  }
-  hideInputError(formEl, inputEl, options);
-}
-
-function hasInvalidInput(inputList) {
-  return !inputList.every((inputEl) => inputEl.validity.valid);
-}
-
-function toggleButtonState(inputEls, submitButton, { inactiveButtonClass }) {
-  if (hasInvalidInput(inputEls)) {
-    submitButton.classList.add(inactiveButtonClass);
-    submitButton.disabled = true;
-    return;
-  }
-  submitButton.classList.remove(inactiveButtonClass);
-  submitButton.disabled = false;
-}
-
-function setEventListeners(formEl, options) {
-  const { inputSelector } = options;
-  const inputEls = Array.from(formEl.querySelectorAll(inputSelector));
-  const submitButton = formEl.querySelector(options.submitButtonSelector);
-  inputEls.forEach((inputEl) => {
-    inputEl.addEventListener("input", (e) => {
-      checkInputValidity(formEl, inputEl, options);
-      toggleButtonState(inputEls, submitButton, options);
-    });
-  });
-}
-
-function enableValidation(options) {
-  const formEls = Array.from(document.querySelectorAll(options.formSelector));
-  formEls.forEach((formEl) => {
-    formEl.addEventListener("submit", (e) => {
-      e.preventDefault();
-    });
-    setEventListeners(formEl, options);
-  });
-}
-
 const config = {
   formSelector: ".modal__form",
   inputSelector: ".modal__input",
@@ -258,4 +201,8 @@ const config = {
   errorClass: "modal__error_visible",
 };
 
-enableValidation(config);
+const addCardFormValidator = new FormValidator(config, "#add-card-form");
+const editCardFormValidator = new FormValidator(config, "#profile-edt-modal");
+
+addCardFormValidator.enableValidation();
+editCardFormValidator.enableValidation();
