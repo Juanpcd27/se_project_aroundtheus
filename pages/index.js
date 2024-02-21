81053760d38c35bdfdc60e38bd77cf887ef6e01d
JuanCarrion <Juanpablocarriondiaz@gmail.com>
2024-02-21 11:08:17 -0600

    p7.8

--- pages/index.js
+++ pages/index.js
@@ -92,33 +92,6 @@ function closeModalEscape(e) {
 /*                                  card.js                                     */
 /* -----------------------------------------------------------------------------*/
 
-const cardData = [
-  {
-    name: "Yosemite Valley",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
-  },
-  {
-    name: "Lake Louise",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
-  },
-  {
-    name: "Bald Mountains",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
-  },
-  {
-    name: "Latemar",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
-  },
-  {
-    name: "Vanoise National Park",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
-  },
-  {
-    name: "Lago di Braies",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
-  },
-];
-
 function handleImageClick(name, link) {
   modalImagePreviewLink.src = link;
   modalImagePreviewLink.alt = name;
@@ -126,9 +99,13 @@ function handleImageClick(name, link) {
   openModal(modalPreviewImage);
 }
 
-function renderCard(cardData, wrapper) {
-  const card = new Card(cardData, "#card-template", handleImageClick);
+function createCard() {
+  const card = new Card(initialCards, "#card-template", handleImageClick);
   const cardElement = card.getView();
+}
+
+function renderCard(initialCards, wrapper) {
+  const cardElement = createCard(initialCards);
   wrapper.prepend(cardElement);
 }
 
